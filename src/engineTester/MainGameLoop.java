d32aa03228cadd2b54195ed7bbbb15090d990633
Stefan Rink <srink@gmx.de>
2017-04-17 19:41:06 +0200

    shaders added

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -6,6 +6,7 @@ import renderEngine.DisplayManager;
 import renderEngine.Loader;
 import renderEngine.RawModel;
 import renderEngine.Renderer;
+import shaders.StaticShader;
 
 public class MainGameLoop {
 
@@ -15,6 +16,7 @@ public class MainGameLoop {
 		
 		Loader loader = new Loader();
 		Renderer renderer = new Renderer();
+		StaticShader shader = new StaticShader();
 		
 		float[] vertices = {
 				-0.5f, 0.5f, 0f,
@@ -31,9 +33,12 @@ public class MainGameLoop {
 		
 		while(!Display.isCloseRequested()){
 			renderer.prepare();
+			shader.start();
 			renderer.render(model);
+			shader.stop();
 			DisplayManager.updateDisplay();
 		}
+		shader.cleanUp();
 		loader.cleanUp();
 		DisplayManager.closeDisplay();
 	}
--- src/renderEngine/Renderer.java
+++ src/renderEngine/Renderer.java
@@ -8,7 +8,7 @@ public class Renderer {
 	
 	public void prepare(){
 		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
-		GL11.glClearColor(1, 0, 0, 1);
+		GL11.glClearColor(1, 1, 0, 1);
 	}
 	
 	public void render(RawModel model){
